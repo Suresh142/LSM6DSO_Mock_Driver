278e7829238c8a38457b27e51a77656cf03bc1e3
Suresh Kumar <114408369+Suresh142@users.noreply.github.com>
2025-08-16 14:27:34 +0530

    Update mock_i2c.h

--- include/mock_i2c.h
+++ include/mock_i2c.h
@@ -1,1 +1,19 @@
+#ifndef MOCK_I2C_H
+#define MOCK_I2C_H
 
+#include <stdint.h>
+
+#define MOCK_I2C_MAX_WRITES 64
+
+typedef struct
+{
+    uint8_t dev, reg, val;
+} i2c_write_op_t;
+
+extern i2c_write_op_t mock_i2c_writes[MOCK_I2C_MAX_WRITES];
+extern int mock_i2c_write_count;
+
+void mock_i2c_set_read_block(uint8_t start_reg, const uint8_t *data, uint16_t len);
+void mock_i2c_reset(void);
+
+#endif
